2f47503e84c1a2172305338962f1d66650131053
prajapatisweta <s.p.2410.s.p@gmail.com>
2022-11-11 22:14:55 +0530

    changes

--- mongo_php/connectToDb.php
+++ mongo_php/connectToDb.php
@@ -1,25 +1,22 @@
-<?php 
-   // connect to mongodb 
-   $m = new MongoClient(); 
-   echo "Connection to database successfully"; 
-  
-   // select a database 
-   $db = $m->mydb; 
-   echo "Database mydb selected"; 
-   $collection = $db->mycol; 
-   echo "Collection selected succsessfully"; 
-    
-   // now remove the document 
-   $collection->remove(array("title"=>"MongoDB Tutorial")); 
-   echo "Documents deleted successfully"; 
-    
-   // now display the available documents 
-   $cursor = $collection->find(); 
-  
-   // iterate cursor to display title of documents 
-   echo "Updated document"; 
-  
-   foreach ($cursor as $document) { 
-      echo $document["title"] . "\n"; 
-   } 
+<?php
+   // connect to mongodb
+   $m = new MongoClient();
+   echo "Connection to database successfully";
+	
+   // select a database
+   $db = $m->mydb;
+   echo "Database mydb selected";
+   $collection = $db->mycol;
+   echo "Collection selected succsessfully";
+	
+   $document = array( 
+      "title" => "MongoDB", 
+      "description" => "database", 
+      "likes" => 100,
+      "url" => "http://www.mongo.com",
+      "by" => "NoSQL"
+   );
+	
+   $collection->insert($document);
+   echo "Document inserted successfully";
 ?>
